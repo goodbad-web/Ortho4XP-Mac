9e87f337c6958aa2a104a8286fdd906be77ae09a
goodbad-web <goodbadhappysad1051@gmail.com>
2026-05-16 16:12:49 +0900

    chore: update default config settings for overpass server and neural upscaling while updating configuration variable lists

--- src/O4_Config_Utils.py
+++ src/O4_Config_Utils.py
@@ -451,6 +451,7 @@ list_global_cfg = (
     + list_mesh_vars
     + list_mask_vars
     + list_dsf_vars
+    + list_other_vars
 )
 
 ################################################################################
